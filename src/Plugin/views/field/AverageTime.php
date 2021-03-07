0fa6526d42d5b8facacaa9b03bf5c732118e3633
Big Fish <54217807@qq.com>
2021-03-07 11:25:34 +0800

    Views field

---
+++ src/Plugin/views/field/AverageTime.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\dyniva_matomo\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Defines custom matomo analytics field.
+ *
+ * @ViewsField("dyniva_matomo_average_time")
+ */
+class AverageTime extends FieldPluginBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function defineOptions() {
+    return parent::defineOptions();
+  }
+
+  /**
+   * @param $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function render(ResultRow $row) {
+    $entity = $this->getEntity($row);
+    return $entity->matomo_visits_summary ? $entity->matomo_visits_summary->avg_time_on_site : '';
+  }
+
+}
